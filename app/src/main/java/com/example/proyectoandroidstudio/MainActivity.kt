29083eedccd2c4eeb53a2d702eee18a4f6e01352
Yerko Navarro <yerkonavarro7@gmail.com>
2025-10-08 23:06:34 -0300

    navegacion

--- app/src/main/java/com/example/proyectoandroidstudio/MainActivity.kt
+++ app/src/main/java/com/example/proyectoandroidstudio/MainActivity.kt
@@ -5,34 +5,41 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.compose.rememberNavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.proyectoandroidstudio.ui.theme.HomeScreen
 import com.example.proyectoandroidstudio.ui.theme.ProyectoAndroidStudioTheme
+import com.example.proyectoandroidstudio.ui.theme.RegistroScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Myapp()
+            MyApp()
         }
     }
 }
 
 @Composable
-fun Myapp(){
+fun MyApp() {
     val navController = rememberNavController()
-    NavHost(navController= navController, startDestination = "home"){
-        composable("home"){HomeScreen(navController)}
-
+    
+    NavHost(
+        navController = navController,
+        startDestination = "home"
+    ) {
+        composable("home") { 
+            HomeScreen(navController = navController) 
+        }
+        composable("registro") { 
+            RegistroScreen(navController = navController) 
+        }
     }
 }
 
--- app/src/main/java/com/example/proyectoandroidstudio/ui/theme/RegistroScreen.kt
+++ app/src/main/java/com/example/proyectoandroidstudio/ui/theme/RegistroScreen.kt
@@ -1,28 +1,37 @@
 package com.example.proyectoandroidstudio.ui.theme
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.proyectoandroidstudio.R
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.TopAppBar
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.compose.material3.TopAppBar
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import com.example.proyectoandroidstudio.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeScreen(navController: NavController) {
+fun RegistroScreen(navController: NavController) {
+
 
     Scaffold (
         topBar = {
             TopAppBar(title = {Text(text = "pasteleria mil sabores")})
+
         }
 
     ){innerPadding ->
@@ -34,23 +43,12 @@ fun HomeScreen(navController: NavController) {
             verticalArrangement = Arrangement.spacedBy(20.dp)
         ){
 
-            /* ajustar a medida del titulo */
-            Image(
 
-                painter = painterResource(id = R.drawable.logo),
-                contentDescription = "Logo App",
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(200.dp),
-                contentScale = ContentScale.Fit
-
-            )
             Spacer(modifier = Modifier.height(90.dp))
             Text(text = "Bienvenido")
-            Button(onClick = { navController.navigate("registro")}) {
+            Button(onClick = {/*accion futura*/}) {
                 Text("Ingresar")
             }
-
         }
 
 
@@ -60,6 +58,11 @@ fun HomeScreen(navController: NavController) {
 
     }
 
+
+
+
 }
+
+
 
 

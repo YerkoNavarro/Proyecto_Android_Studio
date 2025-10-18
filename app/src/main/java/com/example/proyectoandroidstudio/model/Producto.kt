dbe0d8019e15606c6cbac4240e29ec8f16a9397f
YerkoNavarro <yerkonavarro7@gmail.com>
2025-10-17 22:40:28 -0300

    agrega producto

---
+++ app/src/main/java/com/example/proyectoandroidstudio/model/Producto.kt
@@ -0,0 +1,10 @@
+package com.example.proyectoandroidstudio.model
+
+data class Producto(
+    val id: Int,
+    val nombre: String,
+    val precio: Double,
+    val descripcion: String,
+    val imagen: String
+
+)
